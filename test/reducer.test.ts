47391088a13fc2a791bed1c85f5f60f604f98e23
Lukas Würzburger <lukas.wuerzburger@truffls.com>
2020-07-20 21:17:22 +0200

    test: clean up reducer test

--- test/reducer.test.ts
+++ test/reducer.test.ts
@@ -4,7 +4,7 @@ import { initalizeGame } from "../src/game";
 describe("reducer", () => {
   describe("when PLAY_REGULAR_CARD action", () => {
     const state = initalizeGame(2);
-    const cardToPlay = state.players[state.playersTurnIndex].hand[0];
+    const cardToPlay = state.players[0].hand[0];
     const newState = reducer(state, {
       type: "PLAY_REGULAR_CARD",
       payload: cardToPlay,
@@ -12,12 +12,12 @@ describe("reducer", () => {
 
     test("should remove card from current players hand", () => {
       expect(
-        newState.players[state.playersTurnIndex].hand.includes(cardToPlay)
+        newState.players[0].hand.includes(cardToPlay)
       ).toBeFalsy();
     });
 
     test("should set next player", () => {
-      expect(newState.playersTurnIndex).toEqual(state.playersTurnIndex + 1);
+      expect(newState.playersTurnIndex).toEqual(1);
     });
 
     test("should append card to stack", () => {
