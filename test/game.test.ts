bd9650331ac9d135a6c2a69b3f93d67d9da267af
Lukas Würzburger <lukas.wuerzburger@truffls.com>
2020-07-21 11:29:28 +0200

    test: card stack and game state more granular

--- test/game.test.ts
+++ test/game.test.ts
@@ -1,4 +1,4 @@
-import { initalizeGame } from "../src/game";
+import { initalizeGame, createStack } from "../src/game";
 
 describe("game", () => {
   test("initalize should give players 7 cards", () => {
@@ -8,9 +8,18 @@ describe("game", () => {
     }
   });
 
+  test("card stack should have 32 cards", () => {
+    const allCards = createStack()
+    expect(allCards).toHaveLength(32);
+  });
+
   test("initalize should have remaining cards in stack after cards are dealt", () => {
-    const gameState = initalizeGame(3);
-    expect(gameState.stack).toHaveLength(32 - 3 * 7);
+    let gameState = initalizeGame(2);
+    expect(gameState.stack).toHaveLength(18);
+    gameState = initalizeGame(3);
+    expect(gameState.stack).toHaveLength(11);
+    gameState = initalizeGame(4);
+    expect(gameState.stack).toHaveLength(4);
   });
 
   test("initialize game should throw when number of players is not between 2 and 4", () => {
