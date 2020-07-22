2283ce1e0b5a224f010eddd61b6d9ac612964ee8
Emanuel Schmitt <emanuel.schmitt@me.com>
2020-07-22 20:46:04 +0200

    feat: setup client package

---
+++ packages/maumau-client/src/main-root.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainRoot() {
+  return <h1>Hello World 123 23423</h1>;
+}
+
+export default MainRoot;
---
+++ packages/maumau-client/src/main.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainRoot from './main-root';
+
+const element = React.createElement(MainRoot);
+ReactDOM.render(element, document.getElementById('main'));
