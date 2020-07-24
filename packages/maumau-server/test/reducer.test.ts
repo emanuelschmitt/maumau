76a36bda2dc2f7e130dbef0f1bb4a2e2e4bdc91c
Lukas Würzburger <lukas.wuerzburger@truffls.com>
2020-07-24 11:58:31 +0200

    test: ACCEPT_PENDING_SEVENS should draw 2 more cards

--- packages/maumau-server/test/reducer.test.ts
+++ packages/maumau-server/test/reducer.test.ts
@@ -1,8 +1,12 @@
 import { initalizeGame } from '../src/game/game';
 import { reducer } from '../src/game/reducer';
 import { ActionType } from '../src/models/action';
+import Card from '../src/models/card';
+import { Suit } from '../src/models/suit';
+import { Rank } from '../src/models/rank';
 
 describe('reducer', () => {
+  
   describe('when PLAY_REGULAR_CARD action', () => {
     const state = initalizeGame(2);
     const cardToPlay = state.players[0].hand[0];
@@ -22,5 +26,21 @@ describe('reducer', () => {
     test('should append card to stack', () => {
       expect(newState.stack.includes(cardToPlay)).toBeTruthy();
     });
+  });
+
+  describe("when ACCEPT_PENDING_SEVENS action", () => {
+
+    var state = initalizeGame(2);
+    const cardsCount = state.players[1].hand.length
+    state = reducer(state, {
+      type: ActionType.PLAY_SEVEN,
+      payload: new Card(Suit.CLUBS, Rank.SEVEN),
+    })
+    state = reducer(state, {
+      type: ActionType.ACCEPT_PENDING_SEVENS,
+    });
+    test('player should have 2 more cards', () => {
+      expect(state.players[1].hand).toHaveLength(cardsCount + 2);
+    })
   });
 });
