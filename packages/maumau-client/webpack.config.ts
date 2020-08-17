4f3ac1e5e76e36055ad8e59506c6ffffbd6db6aa
Emanuel Schmitt <emanuel.schmitt@me.com>
2020-08-17 22:27:52 +0200

    chore: update dependencies

--- packages/maumau-client/webpack.config.ts
+++ packages/maumau-client/webpack.config.ts
@@ -49,6 +49,8 @@ export default (): webpack.Configuration => {
                   '@babel/preset-typescript',
                 ],
                 plugins: [
+                  '@babel/plugin-proposal-nullish-coalescing-operator',
+                  '@babel/plugin-proposal-optional-chaining',
                   '@babel/proposal-class-properties',
                   '@babel/proposal-object-rest-spread',
                   'babel-plugin-styled-components',
